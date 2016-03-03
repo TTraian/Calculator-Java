1ba4a0df0f60f8c01864cf71a583ba1a694bc30f
TTraian <tanase_traian95@yahoo.com>
2016-03-03 21:21:17 +0200

    Creare mesaj

---
+++ src/Design.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Design extends JFrame {
+	private JTextField input[];
+	private JButton Suma,Produs,Exit;
+	private JTextField rezultat;
+	private JLabel label_input[],label_rezultat;
+
+	public Design() {
+		setTitle("Calculator");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ src/Operatii.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Operatii {
+	
+	public double adunare(double a, double b){
+		double suma = a + b;
+		return suma;
+	}
+	
+	public double inmultire(double a, double b){
+		double produs = a + b;
+		return produs;
+	}
+}
